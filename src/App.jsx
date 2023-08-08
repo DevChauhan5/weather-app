50485c793238fca5086beee9abda14230554bf62
1tasveer <141699383+1tasveer@users.noreply.github.com>
2023-08-08 08:30:26 +0000

    mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
-import "./App.css"; // Make sure to import your CSS for styling
+import "./App.css"; 
 
 function App() {
   const [city, setCity] = useState("");
@@ -54,23 +54,23 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center app-container">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <Toaster />
 
-      <h1 className="text-5xl font-bold text-center mt-5 app-title">Weather App</h1>
+      <h1 className="text-5xl font-bold text-center mt-5">Weather App</h1>
 
-      <div className="text-center bg-gray-100 border-2 border-black rounded-2xl mt-6 p-4 app-card">
-        <div className="m-auto flex flex-row items-center justify-center font-bold bg-gray-200 rounded-2xl w-96 p-4 search-container">
+      <div className="text-center bg-white border-2 border-black rounded-xl mt-6 p-4 w-full max-w-lg">
+        <div className="flex items-center justify-center space-x-4">
           <input
             type="text"
-            className="ml-2 flex-grow outline-none p-2 rounded-lg border-2 border-gray-300 focus:border-blue-500 search-input"
+            className="flex-grow outline-none p-2 rounded-lg border-2 border-gray-300 focus:border-blue-500 w-3/4"
             placeholder="Enter City Name..."
             value={city}
             onChange={(e) => setCity(e.target.value)}
             onKeyDown={getWeather}
           />
           <button
-            className="bg-white border-2 border-gray-300 rounded-lg p-2 m-2 flex items-center justify-center transition hover:bg-blue-500 hover:text-white focus:outline-none search-button"
+            className="bg-blue-500 text-white rounded-lg p-2 w-1/4"
             onClick={fetchWeather}
           >
             {loading ? (
@@ -79,64 +79,47 @@ function App() {
                 height="20"
                 viewBox="0 0 50 50"
                 xmlns="http://www.w3.org/2000/svg"
-                stroke="#000000"
+                stroke="#fff"
+                className="animate-spin"
               >
-                 <svg
-                      width="20"
-                      height="20"
-                      viewBox="0 0 50 50"
-                      xmlns="http://www.w3.org/2000/svg"
-                      stroke="#000000"
-                      className="ml-4"
-                    >
-                      <circle
-                        cx="25"
-                        cy="25"
-                        r="20"
-                        fill="none"
-                        strokeWidth="4"
-                        strokeDasharray="80"
-                        strokeDashoffset="60"
-                      >
-                        <animate
-                          attributeName="stroke-dashoffset"
-                          dur="2s"
-                          values="0;360"
-                          repeatCount="indefinite"
-                        />
-                      </circle>
-                    </svg>
+                <circle
+                  cx="25"
+                  cy="25"
+                  r="20"
+                  fill="none"
+                  strokeWidth="4"
+                  strokeDasharray="80"
+                  strokeDashoffset="60"
+                >
+                  <animate
+                    attributeName="stroke-dashoffset"
+                    dur="2s"
+                    values="0;360"
+                    repeatCount="indefinite"
+                  />
+                </circle>
               </svg>
             ) : (
               "Search"
             )}
           </button>
-          {info ? (
-            <button
-              onClick={resetApp}
-              className="bg-white border-2 border-gray-300 rounded-lg p-1 mr-2 flex items-center justify-center transition hover:bg-blue-500 hover:text-white focus:outline-none reset-button"
-            >
-              Reset
-            </button>
-          ) : null}
         </div>
 
         {info && (
-          <div className="font-bold bg-white mt-4 p-4 rounded-2xl weather-details">
-            <div className="p-2 relative">
-              <h2 className="text-lg font-semibold mb-2">Weather Details:</h2>
-              <h1 className="text-3xl m-2">{info.location.name}</h1>
-              <div className="flex items-center justify-center">
-                <img
-                  src={`https:${info.current.condition.icon}`}
-                  alt={info.current.condition.text}
-                  className="w-16 h-16"
-                />
-                <h1 className="text-3xl m-2">
-                  {info.current.temp_c}°C / {info.current.temp_f}°F
-                </h1>
-              </div>
-              <p className="text-lg m-2">
+          <div className="mt-4">
+            <h2 className="text-lg font-semibold mb-2">Weather Details:</h2>
+            <h1 className="text-3xl mb-2">{info.location.name}</h1>
+            <div className="flex items-center justify-center">
+              <img
+                src={`https:${info.current.condition.icon}`}
+                alt={info.current.condition.text}
+                className="w-16 h-16"
+              />
+              <h1 className="text-3xl ml-2">
+                {info.current.temp_c}°C / {info.current.temp_f}°F
+              </h1>
+            </div>
+            <p className="text-lg m-2">
                 Condition: {info.current.condition.text}
               </p>
               <p className="text-lg m-2">
@@ -164,14 +147,22 @@ function App() {
               <p className="text-lg m-2">
                 Last Updated: {info.current.last_updated}
               </p>
-            </div>
           </div>
         )}
 
         {!info && !loading && (
-          <div className="no-data">
+          <div className="no-data mt-4">
             <p>Search for a city to get weather information.</p>
           </div>
+        )}
+
+        {info && (
+          <button
+            onClick={resetApp}
+            className="bg-blue-500 text-white rounded-lg p-2 mt-4 w-full"
+          >
+            Reset
+          </button>
         )}
       </div>
     </div>
